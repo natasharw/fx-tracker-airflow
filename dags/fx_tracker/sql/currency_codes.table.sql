4eec5b478eeade1201022393c7d3266dd2a51467
Natasha Wilson <wilson.natasharose@gmail.com>
2020-04-16 18:00:33 +0100

    store create table syntax for first postgres tables

---
+++ dags/fx_tracker/sql/currency_codes.table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE alphavantage.currency_codes (
+	cc_currency_code VARCHAR(3),
+	cc_currency_name VARCHAR(255)
+)
+
---
+++ dags/fx_tracker/sql/daily_exchange_rates.table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE alphavantage.daily_exchange_rates (
+    der_date DATE,
+    der_from_currency VARCHAR(3),
+    der_to_currency VARCHAR(3),
+    der_open REAL,
+    der_high REAL,
+    der_low REAL,
+    der_close REAL,
+    der_created_at TIMESTAMP WITHOUT TIME ZONE,
+    der_updated_at TIMESTAMP WITHOUT TIME ZONE,
+    der_deleted_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL
+);
